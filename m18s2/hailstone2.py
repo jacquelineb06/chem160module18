59a6ffbf009eb507c0334055bb8417eda9cd76cd
jacquelineb06 <jbustamante22@ucmerced.edu>
2020-11-28 17:26:56 -0800

    first commit

---
+++ m18s2/hailstone2.py
@@ -0,0 +1,15 @@
+import sys
+if not 'N' in locals():
+	print("Variable N not defined")
+	sys.ext()
+steps=0
+while N !=1:
+	print (N)
+	steps +=1
+	if N%2 == 0:
+		N=N//2
+	else:
+		N = 3*N+1
+print ("steps=",steps)
+
+
